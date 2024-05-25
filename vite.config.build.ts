54c188e249de04924f2133b2cc01db40428be507
samatitanz <pymzjw@163.com>
2024-05-25 14:49:42 +0800

    feat:1

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import babel from "@rollup/plugin-babel";
 
 // Production config
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      include: ["src/**/*"],
+    }),
+  ],
   build: {
     outDir: "dist",
   },
