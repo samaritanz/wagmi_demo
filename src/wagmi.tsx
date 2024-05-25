05d992ce72f8fc2dc01db3d33e4f1e2fca3d22d9
samatitanz <pymzjw@163.com>
2024-05-25 10:35:49 +0800

    feat:1

--- src/wagmi.tsx
+++ src/wagmi.tsx
@@ -1,8 +1,8 @@
-import { http, createConfig } from 'wagmi';
-import { base, mainnet, optimism } from 'wagmi/chains';
-import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors';
+import { http, createConfig } from "wagmi";
+import { base, mainnet, optimism } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
-const projectId = '18221e9887d663300020eeb79ed595d6';
+const projectId = "18221e9887d663300020eeb79ed595d6";
 
 export const config = createConfig({
   chains: [mainnet, optimism, base],
@@ -12,4 +12,5 @@ export const config = createConfig({
     [optimism.id]: http(),
     [base.id]: http(),
   },
+  ssr: true,
 });
